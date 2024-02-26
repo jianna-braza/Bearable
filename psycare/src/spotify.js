ca5807c7058312171bda9a5a5af48ccad5ac761d
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-02-26 00:58:27 -0800

    add progress bars

--- psycare/src/spotify.js
+++ psycare/src/spotify.js
@@ -1,13 +1,38 @@
-import React from 'react';
+import React, { useEffect} from 'react';
 import { Link } from 'react-router-dom';
+import "./spotify.js";
 
 export default function SpotifyPage(props) {
+
+    useEffect(() => {
+        // Create an iframe element
+        const iframe = document.createElement('iframe');
+    
+        // Set attributes for the iframe
+        iframe.setAttribute('src', 'https://open.spotify.com/embed/playlist/1HVP7E8RkpieiGSxvATJbU?utm_source=generator');
+        iframe.setAttribute('width', '100%');
+        iframe.setAttribute('height', '152');
+        iframe.setAttribute('frameborder', '0');
+        iframe.setAttribute('allowfullscreen', '');
+        iframe.setAttribute('allow', 'autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture');
+        iframe.setAttribute('loading', 'lazy');
+    
+        // Apply styles to the iframe
+        iframe.style.borderRadius = '12px';
+    
+        // Append the iframe to the specified element
+        document.getElementById('spotifyPlayer').appendChild(iframe);
+      }, []);
+
   return (
     <div>
       <main>
         <h2>Spotify Page</h2>
         <Link to='/homepage'>home page</Link>
         <Link to='/taskpage'>task page</Link>
+
+        <div id="spotifyPlayer"></div>
+
       </main>
     </div>
   )
--- psycare/src/stats.js
+++ psycare/src/stats.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./stats.css";
 
 export default function StatsPage(props) {
   return (
@@ -8,6 +9,27 @@ export default function StatsPage(props) {
         <h2>Stats Page</h2>
         <Link to='/homepage'>home page</Link>
         <Link to='/taskpage'>task page</Link>
+
+
+
+        <div class="progressBars">
+            <svg>
+                <circle class="bg" cx="57" cy="57" r="52" />
+                <circle class="meter-1" cx="57" cy="57" r="52" />
+                <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black">Personal</text>
+            </svg>
+            <svg>
+                <circle class="bg" cx="57" cy="57" r="52" />
+                <circle class="meter-2" cx="57" cy="57" r="52" />
+                <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black">School</text>
+            </svg>
+            <svg>
+                <circle class="bg" cx="57" cy="57" r="52" />
+                <circle class="meter-3" cx="57" cy="57" r="52" />
+                <text x="50%" y="50%" dominant-baseline="middle" text-anchor="middle" fill="black">Work</text>
+            </svg>
+        </div>
+
       </main>
     </div>
   )
