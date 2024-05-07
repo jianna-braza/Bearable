c22c24ab2c32e3f2bdbb065e489f0c55f9360146
mmel4nie <87841303+mmel4nie@users.noreply.github.com>
2024-05-07 16:03:19 -0700

    edit nav style

--- psycare/src/Navbar.js
+++ psycare/src/Navbar.js
@@ -5,7 +5,28 @@ import './Navbar.css';
 export default function Navbar(props) {
   return (
     <div id="navbar">
-      <div className='d-flex'>
+
+      <div className="nav-title">
+        <h1>Bearable</h1>
+      </div>
+
+
+      <nav>
+        <div className="nav-links">
+          <a id="first-link" href="/homepage">Home</a>
+          <a href="/taskmanager">Task Manager</a>
+          <a href="/stats">Achievements</a>
+          <a href="/homepage" className="nav-last-link">Resources</a>
+        </div>
+      </nav>
+      
+
+
+
+
+
+
+      {/* <div className='d-flex'>
         <h1><strong>Psycare</strong></h1>
           <nav>
             <ul className="menu mb-4 d-flex justify-content-end">
@@ -23,7 +44,9 @@ export default function Navbar(props) {
               </li>
             </ul>
           </nav>
-      </div>
+      </div> */}
+
+
     </div>
     
 
